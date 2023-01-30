088329851d8f3c95fecd0c88dbfad7c7d838ea5f
tentato <alepaw99@gmail.com>
2023-01-31 00:55:13 +0100

    i need sleep

--- classification_OvO_SelectKBest_test.py
+++ classification_OvO_SelectKBest_test.py
@@ -7,6 +7,7 @@ from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.svm import SVC
+from scipy.stats import ttest_rel
 print("")
 
 main_folder = 'results/KrzysztofJ_all/'
@@ -27,7 +28,7 @@ scores = []
 for k in range(0, 16):
     method_val = []
     for filename in filenames: 
-        # file_object.write(filename+"\n")
+        # file_object.write(f'k = {k}\n')
         dataset = pd.read_csv(main_folder+filename, sep=",", decimal=".", header=None)
         X = dataset.iloc[:, 0:-1].values
         y = dataset.iloc[:, -1].values.astype(int)
@@ -53,13 +54,26 @@ for k in range(0, 16):
             y_train_fold, y_test_fold = y[train_index], y[test_index]
             ovo.fit(x_train_fold, y_train_fold)
             predict = ovo.predict(x_test_fold)
-            mean_arr = np.append(mean_arr, metrics.accuracy_score(y_test_fold, predict))
-        method_val.append(round(np.mean(mean_arr),2))
+            mean_arr.append(metrics.accuracy_score(y_test_fold, predict))
+        # method_val.append(round(np.mean(mean_arr),2))
+        method_val.append(mean_arr)
     # print(method_val)
     scores.append(method_val)
 
     file_object.write(f'{method_val}\n')
 
+alfa = .05
 
+print(f'test: {scores[0]}')
+
+for k in range(16, 1, -1):
+    t_statistic = np.zeros((len(filenames), len(filenames)))
+    p_value = np.zeros((len(filenames), len(filenames)))
+
+    for i in range(len(filenames)):
+        for j in range(len(filenames)):
+            t_statistic[i, j], p_value[i, j] = ttest_rel(scores[k][i], scores[k][j])
+    # print("\n\nt-statistic for k = {k}\n", t_statistic, "\n\np-value:\n", p_value)
+    file_object.write(f'\n\nt-statistic for k = {k}\n{t_statistic}\n\np-value:\n {p_value}')
 
 file_object.close()
