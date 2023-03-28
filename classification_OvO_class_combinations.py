647fda119b32363e5b9229e185ef798cd0d6112a
tentato <alepaw99@gmail.com>
2023-03-28 20:34:02 +0200

    classes reduction/combination implemented

--- classification_OvO_class_combinations.py
+++ classification_OvO_class_combinations.py
@@ -17,7 +17,8 @@ main_folder = 'results/Barbara_13_05_2022_AB/'
 # filenames = ["features_MAV.csv","features_SSC.csv","features_VAR.csv","features_WL.csv","features_ZC.csv"]
 
 filenames = os.listdir(main_folder)
-classes = ['1','2','3','4','5']
+classes = [1,2,3,4,5]
+# classes = ['1','2','3','4','5']
 number_of_classes = len(classes)
 
 list_combinations_classes = list()
@@ -33,10 +34,9 @@ for filename in filenames:
         print(filename)
         file_object.write(f'\n\n{filename}')
         for idx, class_combination in enumerate(list_combinations_classes):
-            class_combination_name = "_".join(class_combination)
-            file_object.write(f'\nClass combination: {class_combination_name}')
+            file_object.write(f'\n\nClass combination: {str(class_combination)}')
             dataset = pd.read_csv(main_folder+filename, sep=",", decimal=".", header=None)
-            
+            dataset = dataset[dataset.iloc[:, -1].isin(class_combination)]
             X = dataset.iloc[:, 0:-1].values
             y = dataset.iloc[:, -1].values.astype(int)
             kfold = RepeatedStratifiedKFold(n_splits=2, n_repeats=5,random_state=11)
@@ -55,7 +55,11 @@ for filename in filenames:
                 ovo.fit(x_train_fold, y_train_fold)
                 predict = ovo.predict(x_test_fold)
                 ###Evaluating Prediction Accuracy
+                if round(metrics.balanced_accuracy_score(y_test_fold, predict),2) < 0.6:
+                    file_object.write(f'\nAccuracy too low, skipping...: {round(metrics.balanced_accuracy_score(y_test_fold, predict),2)}')
+                    break
                 file_object.write(f'\nRFC OvO Acc: {round(metrics.balanced_accuracy_score(y_test_fold, predict),2)}')
                 print("RFC OvO Acc: ",round(metrics.balanced_accuracy_score(y_test_fold, predict),2))
                 balanced_accuraccy_array.append(round(metrics.balanced_accuracy_score(y_test_fold, predict),2))
-            file_object.write(f'\nRFC OvO Mean Acc: {round(np.mean(balanced_accuraccy_array),2)}')
+            if len(balanced_accuraccy_array) > 0:
+                file_object.write(f'\nMean Accuracy: {round(np.mean(balanced_accuraccy_array),2)}')
