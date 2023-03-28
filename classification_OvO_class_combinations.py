b77bae87da8d0f2ea04965778e91d81141e82d6b
tentato <alepaw99@gmail.com>
2023-03-28 18:57:55 +0200

    combinations

--- classification_OvO_class_combinations.py
+++ classification_OvO_class_combinations.py
@@ -17,11 +17,11 @@ main_folder = 'results/Barbara_13_05_2022_AB/'
 # filenames = ["features_MAV.csv","features_SSC.csv","features_VAR.csv","features_WL.csv","features_ZC.csv"]
 
 filenames = os.listdir(main_folder)
-classes = [1,2,3,4,5]
+classes = ['1','2','3','4','5']
 number_of_classes = len(classes)
 
 list_combinations_classes = list()
-for n in range(len(classes) + 1):
+for n in range(2, len(classes) + 1):
     list_combinations_classes += list(combinations(classes, n))
 list_combinations_classes = list_combinations_classes[::-1] # reverse tuple
 
@@ -33,7 +33,10 @@ for filename in filenames:
         print(filename)
         file_object.write(f'\n\n{filename}')
         for idx, class_combination in enumerate(list_combinations_classes):
+            class_combination_name = "_".join(class_combination)
+            file_object.write(f'\nClass combination: {class_combination_name}')
             dataset = pd.read_csv(main_folder+filename, sep=",", decimal=".", header=None)
+            
             X = dataset.iloc[:, 0:-1].values
             y = dataset.iloc[:, -1].values.astype(int)
             kfold = RepeatedStratifiedKFold(n_splits=2, n_repeats=5,random_state=11)
