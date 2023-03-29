4e30c1d153d4b4e102000b30ad7b6703afb02a29
tentato <alepaw99@gmail.com>
2023-03-30 00:28:31 +0200

    wave decomposition prepared

--- signal_channel_filtration.py
+++ signal_channel_filtration.py
@@ -4,11 +4,13 @@ import pandas as pd
 import os
 import pysiology.electromyography as electromyography
 from itertools import combinations
-import PyWavelets as pywt
+import pywt
 
 # This data set contains only 8 channels of EMG signals (no MMG data)
 
 print(" ")
+# print(pywt.wavelist(kind='discrete'))
+# exit()
 
 results_folder = 'results/Barbara_13_05_2022_AB/'
 main_folder = 'C:/Users/alepa/Desktop/MGR/datasets/Barbara_13_05_2022_AB/'
@@ -24,9 +26,13 @@ for class_number in classes_array:
         new_df = pd.DataFrame()
         i=0
         for (columnName, columnData) in dataset.iteritems():
-            new_df[str(i)] = columnData
-            new_df[str(i+1)] = columnData
-            i+=2
+            # new_df[str(i)] = columnData
+            # new_df[str(i+1)] = columnData
+            # i+=2
+            new = pywt.wavedec(columnData, 'coif1', level=3)
+            new = np.array(new)
+            print(new.shape)
+            exit()
         
     print(f"Class {class_number} processed...")
 
