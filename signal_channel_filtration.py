f9de16be2f4e9a3087c3744306f0b2ce06674643
tentato <alepaw99@gmail.com>
2023-04-17 23:10:34 +0200

    wavelet decomposition finished

--- signal_channel_filtration.py
+++ signal_channel_filtration.py
@@ -10,8 +10,6 @@ from scipy.interpolate import interp1d
 # This script performs wavelet transfor on all signals in dataset.
 
 print(" ")
-# print(pywt.wavelist(kind='discrete'))
-# exit()
 
 results_folder = 'C:/Users/alepa/Desktop/MGR/datasets/Barbara_13_05_2022_AB_wavdec/'
 main_folder = 'C:/Users/alepa/Desktop/MGR/datasets/Barbara_13_05_2022_AB/'
@@ -22,16 +20,14 @@ for class_num in classes_array:
 
 
 for class_number in classes_array:
+    print(f"Processing files for class {class_number}")
     for file_index, filename in enumerate(os.listdir(main_folder+class_number)):
+        print(f"Processing {filename} file...")
         dataset = pd.read_csv(main_folder+class_number+'/'+filename, sep=";", decimal=",", header=None)
-        new_df = pd.DataFrame()
         new_df_arr = []
         i=0
         for (columnName, columnData) in dataset.items():
-            # convert array to column
             coeffs = pywt.wavedec(columnData, 'db1', level=3)
-            fig, ax = plt.subplots(5, 1, figsize=(10, 13))
-
             new_len = 1000
             f1 = interp1d(np.linspace(0, 1, len(coeffs[0])), coeffs[0], kind='linear')
             rescaled_arr1 = f1(np.linspace(0, 1, new_len))
@@ -42,23 +38,29 @@ for class_number in classes_array:
             f4 = interp1d(np.linspace(0, 1, len(coeffs[3])), coeffs[3], kind='linear')
             rescaled_arr4 = f4(np.linspace(0, 1, new_len))
             
-            x = np.linspace(start = 0, stop = 1000, num = 1000) # for AB
-            ax[0].plot(x, columnData)
-            ax[1].plot(x, rescaled_arr1)
-            ax[2].plot(x, rescaled_arr2)
-            ax[3].plot(x, rescaled_arr3)
-            ax[4].plot(x, rescaled_arr4)
-            ax[0].set_title("EMG")
-            plt.tight_layout()
-            plt.savefig("test.png")
-            exit()
-            new_arr = np.array(new_arr)
-            new_df_arr.append(new_arr)
-        new_df_arr = np.array(new_df_arr)
+            ##### PRINTING SIGNALS #####
+            # fig, ax = plt.subplots(5, 1, figsize=(10, 13))
+            # x = np.linspace(start = 0, stop = 1000, num = 1000) # for AB
+            # ax[0].plot(x, columnData)
+            # ax[1].plot(x, rescaled_arr1)
+            # ax[2].plot(x, rescaled_arr2)
+            # ax[3].plot(x, rescaled_arr3)
+            # ax[4].plot(x, rescaled_arr4)
+            # ax[0].set_title("EMG")
+            # plt.tight_layout()
+            # plt.savefig("test.png")
+
+            new_df_arr.append(rescaled_arr1)
+            new_df_arr.append(rescaled_arr2)
+            new_df_arr.append(rescaled_arr3)
+            new_df_arr.append(rescaled_arr4)
             
-        print(new_df_arr.shape)
-        exit()
-        new_df.to_csv(f"{results_folder}/{class_num}/", header=False, index=False)
+        new_df_arr = np.array(new_df_arr)
+        new_df_arr = np.transpose(new_df_arr)
+        new_df = pd.DataFrame(new_df_arr)
+
+        print(f"Saving decomposition results as {results_folder}{class_number}/{filename}")
+        new_df.to_csv(f"{results_folder}{class_number}/{filename}", header=False, index=False)
         
         
     print(f"Class {class_number} processed...")
