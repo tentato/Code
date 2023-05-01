34428828dfe6feb80c8e6db5b36fb823ad0b63c6
tentato <alepaw99@gmail.com>
2023-05-01 20:10:13 +0200

    problexity experiments

--- problexity_test.py
+++ problexity_test.py
@@ -3,13 +3,6 @@ import matplotlib.pyplot as plt
 import os
 import pandas as pd
 import numpy as np
-from problexity import classification as c
-
-
-# WL - MAV, 1,4,5 
-# 3 Classes, accuracy = 98%
-# ZC - MAV - SSC, 1,3,4 
-# 3 Classes, accuracy = 86%
 
 main_folder = 'results/Barbara_13_05_2022_AB_wavdec/'
 # main_folder = 'results/Barbara_13_05_2022_AB/'
@@ -17,9 +10,14 @@ main_folder = 'results/Barbara_13_05_2022_AB_wavdec/'
 # main_folder = 'results/MK/'
 # filenames = ["features_MAV.csv","features_SSC.csv","features_VAR.csv","features_WL.csv","features_ZC.csv"]
 
+###
 # filename = "features_WL_MAV.csv"
-filename = "features_ZC_MAV_SSC.csv"
+# filename = "features_WL_ZC_MAV.csv"
+# filename = "features_WL_ZC_MAV_SSC.csv"
 # classes = [1,4,5]
+###
+
+filename = "features_ZC_MAV_SSC.csv"
 classes = [1,3,4]
 number_of_classes = len(classes)
 
@@ -33,15 +31,12 @@ dataset = dataset[dataset.iloc[:, -1].isin(classes)]
 X = dataset.iloc[:, 0:-1].values
 y = dataset.iloc[:, -1].values.astype(int)
 
-# Initialize CoplexityCalculator with default parametrization
-# cc = px.ComplexityCalculator(metrics='f1', colors=['#FD0100'], ranges={'FB': 1}, weights=np.ones((1)))
-# cc = px.ComplexityCalculator(metrics='f1')
+
 cc = px.ComplexityCalculator()
 
 # Fit model with data
 cc.fit(X,y)
 print(f"Report: \n{cc.report()}\n")
-print(f"Met: {len(cc._metrics())}\n")
 cc.plot(fig, (1,1,1))
 
 plt.tight_layout()
