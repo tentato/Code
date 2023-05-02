13e6cd32dbc5fd1d581a43d0c032b7d50f5f2bd4
tentato <alepaw99@gmail.com>
2023-05-02 23:10:02 +0200

    roll back to previous

--- problexity_test.py
+++ problexity_test.py
@@ -32,61 +32,13 @@ dataset = dataset[dataset.iloc[:, -1].isin(classes)]
 X = dataset.iloc[:, 0:-1].values
 y = dataset.iloc[:, -1].values.astype(int)
 
-
 cc = px.ComplexityCalculator(multiclass_strategy='ova')
 
-def f1(X, y):
-    """
-    Calculates the Maximum Fisher's discriminant ratio (F1) metric.
-    Measure describes the overlap of feature values in each class.
-    .. math::
-        F1=\\frac{1}{1+max^{m}_{i=1}r_{f_{i}}}
-    :type X: array-like, shape (n_samples, n_features)
-    :param X: Dataset
-    :type y: array-like, shape (n_samples)
-    :param y: Labels
-    :rtype: float
-    :returns: F1 score
-    """
-    
-    X = np.copy(X)
-    y = np.copy(y)
 
-    X_0 = X[y==0]
-    X_1 = X[y==1]
-
-    try:
-        X_0_prop = X_0.shape[0]/X.shape[0]
-        X_1_prop = X_1.shape[0]/X.shape[0]
-    except:
-        return np.nan
-
-    X_0_mean = np.mean(X_0, axis = 0)
-    X_1_mean = np.mean(X_1, axis = 0)
-
-    X_0_std = np.std(X_0, axis = 0)
-    X_1_std = np.std(X_1, axis = 0)
-
-    l = (X_0_prop*X_1_prop*np.power((X_0_mean - X_1_mean),2)) + (X_1_prop*X_0_prop*np.power((X_1_mean - X_0_mean),2))
-    m = (X_0_prop*(np.power(X_0_std,2))) + (X_1_prop*(np.power(X_1_std,2)))
-    # print(l)
-    # print(m)
-    r_all = l/m
-    print(f"R_ALL: {r_all}")
-    print(1 / (1+np.max(r_all)))
-    return 1 / (1+np.max(r_all))
-
-complexity = []
-
-for c in classes:
-    complexity.append(f1(X, (y == c).astype(int)))
-
-print(complexity)
-exit()
 # Fit model with data
-# cc.fit(X,y)
-# print(f"Report: \n{cc.report()}\n")
-# cc.plot(fig, (1,1,1))
+cc.fit(X,y)
+print(f"Report: \n{cc.report()}\n")
+cc.plot(fig, (1,1,1))
 
-# plt.tight_layout()
-# plt.savefig(f"problexity_{filename.split('.')[0]}.png")
+plt.tight_layout()
+plt.savefig(f"problexity_{filename.split('.')[0]}.png")
