0562320f31e11a6c6cd50e17bc6048a22eaec8eb
tentato <alepaw99@gmail.com>
2023-05-18 20:24:48 +0200

    selectkbest tests

--- classification/classification_SelectKBest_test_amp2.py
+++ classification/classification_SelectKBest_test_amp2.py
@@ -5,6 +5,7 @@ from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectKBest
 from sklearn.multiclass import OneVsOneClassifier
+from sklearn.multiclass import OneVsRestClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
@@ -13,7 +14,6 @@ from sklearn.svm import SVC
 from tabulate import tabulate
 from scipy.stats import ttest_rel
 print("")
-
 main_folder = 'dataset_features/amp2_wavdec/'
 
 filenames = os.listdir(main_folder)
@@ -25,22 +25,23 @@ for n in range(2, len(classes) + 1):
     list_combinations_classes += list(combinations(classes, n))
 list_combinations_classes = list_combinations_classes[::-1] # reverse tuple
 
-file_object = open(f'{main_folder}results_OvO_RFC_class_combinations.txt', 'w')
+file_object = open(f'{main_folder}results_Select_K_Best.txt', 'w')
 target_accuracy = 0.6
+size = 16 * 4
 
-clfs = RandomForestClassifier(max_depth=2, random_state=11)
-
-file_object = open('results_Select_K_Best', 'w')
+model = RandomForestClassifier(max_depth=2, random_state=11)
 
 scores = []
 mean_scores = []
 
-for k in range(0, 16):
+for k in range(0, size):
     method_val = []
     mean_method_val = []
     for filename in filenames: 
-        # file_object.write(f'k = {k}\n')
-        dataset = pd.read_csv(main_folder+filename, sep=",", decimal=".", header=None)
+        if "features_" not in filename:
+            print(f"{filename} is not a valid dataset...")
+            continue
+        dataset = pd.read_csv(main_folder+filename, sep=",", decimal=".", header=0)
         X = dataset.iloc[:, 0:-1].values
         y = dataset.iloc[:, -1].values.astype(int)
 
@@ -55,22 +56,25 @@ for k in range(0, 16):
         kfold = RepeatedStratifiedKFold(n_splits=2, n_repeats=5,random_state=11)
         splits = kfold.split(X,y)
 
-        model = clfs['SVM']
-        ovo = OneVsOneClassifier(model)
+        ova = OneVsRestClassifier(model)
 
         mean_arr = []
 
         for n,(train_index,test_index) in enumerate(splits):
             x_train_fold, x_test_fold = X[train_index], X[test_index]
             y_train_fold, y_test_fold = y[train_index], y[test_index]
-            ovo.fit(x_train_fold, y_train_fold)
-            predict = ovo.predict(x_test_fold)
+            ova.fit(x_train_fold, y_train_fold)
+            predict = ova.predict(x_test_fold)
             mean_arr.append(metrics.accuracy_score(y_test_fold, predict))
         mean_method_val.append(round(np.mean(mean_arr),2))
         method_val.append(mean_arr)
     scores.append(method_val)
 
     file_object.write(f'{mean_method_val}\n')
+    break
+
+file_object.close()
+exit()
 
 alfa = .05
 # print(f'test: {scores[0]}')
@@ -115,5 +119,4 @@ for k in range(0,16):
         (names_column, stat_better), axis=1), headers)
     # print("Statistically significantly better:\n", stat_better_table)
     file_object.write(f'\n\nStatistically significantly better:\n{stat_better_table}\n')
-
 file_object.close()
