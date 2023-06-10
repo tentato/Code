bfa96058736c0df6c2dcf7c7fa666ab86fc8902c
tentato <alepaw99@gmail.com>
2023-06-10 14:56:29 +0200

    exp

--- show/measure_values_90.py
+++ show/measure_values_90.py
@@ -34,9 +34,6 @@ fig, ax = plt.subplots(10, 2, figsize=(10, 20))
 ax = ax.reshape(-1)
 
 file_object = open(f'{results_folder}correlation_values.txt', 'w')
-# file_object = open(f'{results_folder}spearman_correlation.txt', 'w')
-# csv_header = 'Class combination;Number of classes;mean_good_accuracy;f1;min_f1;f2;min_f2;f3;min_f3;f4;min_f4;l1;min_l1;l2;min_l2;l3;min_l3;n1;min_n1;n2;min_n2;n4;min_n4'
-# csv_header = 'Class combination;Number of classes;mean_good_accuracy;l1;min_l1;l2;min_l2;l3;min_l3;n1;min_n1;n2;min_n2;n4;min_n4;sum_measures;mean_measures'
 csv_header = 'Class combination;Number of classes;mean_good_accuracy;l1;l2;l3;n1;n2;n4;sum_measures;mean_measures'
 file_object.write(csv_header)  
 
